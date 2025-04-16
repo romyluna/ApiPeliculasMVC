22869a2d4ac05bb58a58782c6dce56cdbc2f3f85
romyluna <134311514+romyluna@users.noreply.github.com>
2025-04-16 10:19:18 -0300

    categoriasController: agrego el borrar peliculas.

--- PeliculasWeb/Controllers/PeliculasController.cs
+++ PeliculasWeb/Controllers/PeliculasController.cs
@@ -4,6 +4,7 @@ using PeliculasWeb.Models;
 using PeliculasWeb.Models.ViewModels;
 using PeliculasWeb.Repositorio.IRepositorio;
 using PeliculasWeb.Utilidades;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace PeliculasWeb.Controllers
@@ -112,7 +113,7 @@ namespace PeliculasWeb.Controllers
             return View(objVM);
         }
 
-
+        //EDITAR:
 
         [HttpGet]
 
@@ -149,21 +150,84 @@ namespace PeliculasWeb.Controllers
             objVM.Pelicula = await _repoPelicula.GetAsync(CT.RutaPeliculasApi,id.GetValueOrDefault());
 
 
+            if (objVM.Pelicula == null)
+            {
+                return NotFound();
+            }
+            return View(objVM);
+        }
+        
+       
+
+        [HttpPost]
+        //para protegerse de ataques CSRF (Cross-Site Request Forgery).Esto ayuda a garantizar que las solicitudes POST
+        //(como enviar formularios) sean legítimas y no generadas maliciosamente desde otro lugar
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(Pelicula pelicula)
+        {
+            //para crear una pelicula necesitamos asignarle una categoria:una lista desplegable con las categorias que hay.
+            IEnumerable<Categoria> ctList = (IEnumerable<Categoria>)await _repoCategoria.GetTodoAsync(CT.RutaCategoriasApi); //casteo ienumerable
+
+            //PeliculasVM es un ViewModel que combina dos cosas: una lista de categorías (listaCategorias)
+            //para mostrar en un control desplegable y un objeto Pelicula que representa los datos de una película
+
+            PeliculasVM objVM = new PeliculasVM()
+            {
+
+                //traigo la lista de categorias aca:
+
+                ListaCategorias = ctList.Select(i => new SelectListItem
+                {
+                    Text = i.Nombre,
+                    Value = i.Id.ToString()
+                }),
+
+                //traigo los datos de pelicula
+                Pelicula = new Pelicula()
+            };
+
+            // Compruebo si el modelo recibido es válido según las validaciones definidas en el modelo Pelicula
+            if (ModelState.IsValid)
+            {
+                //con esto obtengo los archivos subidos desde el formulario 
+                var files = HttpContext.Request.Form.Files;
+                //si se ah subido algun archivo:
+                if (files.Count > 0)
+                {
+                    pelicula.Imagen = files[0];//asignar el iformFile directamente a la propiedad imagen
+                }
+                else
+                {
+                    // Si no se ha subido ningún archivo, retorno la vista con el ViewModel para que el usuario complete los datos
+                    return View(objVM);
+                }
+                //Llamo al repositorio para crear la película en la API correspondiente
+                await _repoPelicula.ActualizarPeliculaAsync(CT.RutaPeliculasApi + pelicula.Id, pelicula);
+                // Redirigir al usuario a la acción Index después de crear exitosamente la película
+                return RedirectToAction(nameof(Index));
+            }
+            // Si el modelo no es válido, retorno la vista con el ViewModel para que el usuario corrija los errores
             return View(objVM);
         }
 
 
+        //BORRAR
 
+        [HttpDelete]
+        //para protegerse de ataques scsc
+        // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var status = await _repoPelicula.BorrarAsync(CT.RutaPeliculasApi, id);
 
+            //si hay status si se pudo borrar 
+            if (status)
+            {
+                return Json(new { success = true, message = "Borrado correctamente" });
+            }
 
-
-
-
-
-
-
-
-
+            return Json(new { success = true, message = "No se pudo borrar" });
+        }
 
     }
 }
